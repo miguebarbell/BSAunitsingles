2630a6db2bbdcd7520a87658085373144e9054ec
Miguel R <contact@migue.pro>
2021-11-24 12:56:50 -0500

    added model field put some defaults to the model

--- ecommerceapi/models/Product.js
+++ ecommerceapi/models/Product.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema({
     title: {type: String, required: true, unique: true},
     desc: {type: String, required: true},
-    img: {type: String, required: true},
-    categories: {type: Array},
+    img: {type: String, required: true, default: "https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-15.png"},
+    categories: {type: Array, default: ["featured"]},
     sku: {type: String},
-    // color: {type: String},
+    model: {type: Array, default: ["all"]},
     price: {type: Number, required: true},
     inStock: {type: Boolean, default: true},
     onHand: {type: Number, default: 1, required:true}
