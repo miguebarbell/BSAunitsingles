6cdaaf1f6e864e35fd78a7cf6de264774f62173f
Miguel  Rojas <contact@migue.pro>
2021-12-16 17:15:19 -0500

    added logout

--- ecommerceapi/models/User.js
+++ ecommerceapi/models/User.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    username: {type: String, required: true, unique: true},
+    // username: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     isAdmin:{type: Boolean, default: false},
+    address: {type: String, required: false},
+    name: {type: String, required: false},
+    lastName: {type: String, required: false}
 
 }, {timestamp: true});
 
