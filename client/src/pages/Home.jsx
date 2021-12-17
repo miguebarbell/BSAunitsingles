995c70c63a3940e193ff65aea917941a1f7d3ff1
Miguel  Rojas <contact@migue.pro>
2021-12-16 20:50:42 -0500

    reset error message when come to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,8 +3,13 @@ import Announcement from "../components/Announcement";
 import Slider from "../components/Slider";
 import Categories from "../components/Categories";
 import FeaturedProducts from "../components/FeaturedProducts";
+import {useDispatch} from "react-redux";
+import {resetLogin} from "../redux/userRedux";
 
 const Home = () => {
+
+    const dispatch = useDispatch()
+    dispatch(resetLogin())
     return (
         <div>
             <Announcement/>
