ed4c5e7a1911345c0f6d6510f094bb2d1b9564fd
Miguel <contact@migue.pro>
2021-12-21 12:15:32 -0500

    added user_Id field for better filtering when querying

--- ecommerceapi/models/Order.js
+++ ecommerceapi/models/Order.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const OrderSchema = new mongoose.Schema({
+    user_Id: {type: String, required: true},
     userId: {type: String, required: true},
     products: [
         {
