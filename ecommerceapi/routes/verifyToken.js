30ad5091f942f443473f8d593b99cf9145148293
Miguel <contact@migue.pro>
2022-01-12 20:10:45 -0500

    created a http request file

--- ecommerceapi/routes/verifyToken.js
+++ ecommerceapi/routes/verifyToken.js
@@ -3,7 +3,8 @@ const jwt = require("jsonwebtoken")
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers.token
     // console.log(authHeader)
-    // console.log(req.body)
+    console.log(req.headers.token)
+    console.log(req.body)
     if (authHeader) {
         const token = authHeader.split(" ")[1];
         jwt.verify(token, process.env.JWT_SEC, (err, user) => {
