f512c9d3cb7cf38d68844fc8d59e53668320b36a
Miguel <contact@migue.pro>
2022-01-26 11:54:59 -0500

    added a image preview for elements

--- client/src/pages/SearchResults.jsx
+++ client/src/pages/SearchResults.jsx
@@ -6,10 +6,13 @@ import {useEffect, useState} from "react";
 import {yellow} from "../components/Navbar"
 import Loading from "../components/Wait";
 
-// TODO: visualizacion de las imagenes
-
 const Tr = styled.tr`
-   background-color: ${({onhand}) => (onhand === 0) ? "rgba(250, 0,0,0.2)" : "rgba(0,0,0,0.1)"};
+   background-color: ${({onhand}) => (onhand <= 0) ? "rgba(250, 0,0,0.2)" : "rgba(0,0,0,0.1)"};
+  &:hover {
+    img {
+      display: block;
+    }
+  }
 `
 const GridResults = styled.table`
   * {
@@ -36,7 +39,20 @@ const GridResults = styled.table`
 
 
 const Image = styled.img`
+  object-fit: cover;
+  border-radius: 7px;
+  display: none;
   position: fixed;
+  top: 50%;
+  right: 20%;
+  transform: translate(0, -50%);
+  background-color: red;
+  min-height: 375px;
+  min-width: 375px;
+  width: 30vh;
+  height: 30vh;
+  max-width: 500px;
+  max-height: 500px;
 `
 const SearchResults = () => {
     const location = useLocation();
@@ -47,9 +63,9 @@ const SearchResults = () => {
             try {
                 const res = await findProduct(query)
                 const sorted = res.data.sort((a, b) => {
-                    if (a.item.onHand === 0 && b.item.onHand > 0) {
+                    if (a.item.onHand <= 0 && b.item.onHand > 0) {
                         return 1
-                    } else if (b.item.onHand === 0 && a.item.onHand > 0) {
+                    } else if (b.item.onHand <= 0 && a.item.onHand > 0) {
                         return -1
                     } else {
                         return 0
@@ -64,7 +80,7 @@ const SearchResults = () => {
         }
         getProducts(query.join('/'))
     }, [products])
-    // console.log(products)
+    console.log(products)
     return (
         <Container style={{backgroundColor:"white"}}>
             {((products.length === 0 && (
@@ -75,28 +91,22 @@ const SearchResults = () => {
             <GridResults>
 
                 {(products.length > 0 && (
-                                   <tr>
-                    <th>#</th>
-                    <th>SKU</th>
-                    <th>Item</th>
-                    <th>Price</th>
-                    <th>On hand</th>
-                </tr>
+                    <tr>
+                        <th>#</th>
+                        <th>SKU</th>
+                        <th>Item</th>
+                        <th>Price</th>
+                        <th>On hand</th>
+                    </tr>
                 ) )}
-                {/*<tr>*/}
-                {/*    <th>#</th>*/}
-                {/*    <th>SKU</th>*/}
-                {/*    <th>Item</th>*/}
-                {/*    <th>Price</th>*/}
-                {/*    <th>On hand</th>*/}
-                {/*</tr>*/}
                 {products.map((item, index) => (
                     <Tr onhand={item.item.onHand}>
-                        <td><Link to={`/product/${item.item._id}`}>{index}</Link></td>
                         <td><Link to={`/product/${item.item._id}`}>{item.item.sku}</Link></td>
                         <td><Link to={`/product/${item.item._id}`}>{item.item.title}</Link></td>
+                        <td><Link to={`/product/${item.item._id}`}>{index}</Link></td>
                         <td><Link to={`/product/${item.item._id}`}>{item.item.price}</Link></td>
                         <td><Link to={`/product/${item.item._id}`}>{item.item.onHand}</Link></td>
+                        <Image src={item.item.img}/>
                     </Tr>
                 ))}
             </GridResults>
