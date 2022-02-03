c34e5f999cb866474ca2089451a2420f28a99ec4
Miguel <contact@migue.pro>
2022-02-02 22:22:00 -0500

    added the weight to the model

--- ecommerceapi/models/Product.js
+++ ecommerceapi/models/Product.js
@@ -3,14 +3,18 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema({
     title: {type: String, required: true, unique: true},
     desc: {type: String, required: true},
-    img: {type: String, required: true, default: "https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-15.png"},
+    img: {
+        type: String,
+        required: true,
+        default: "https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-15.png"
+    },
     categories: {type: Array, default: ["featured"]},
     sku: {type: String},
     model: {type: Array, default: ["all"]},
     price: {type: Number, required: true},
     inStock: {type: Boolean, default: true},
-    onHand: {type: Number, default: 1, required:true}
-
+    onHand: {type: Number, default: 1, required: true},
+    weight: {type: Number}
 
 }, {timestamps: true});
 
